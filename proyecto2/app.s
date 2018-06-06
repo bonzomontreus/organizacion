7be099f29c65615f7e3b31c4d3787954cc44fcb1
MAURO EZEQUIEL PARIS <mparis622@lab29.famaf.unc.edu.ar>
2018-06-06 10:25:31 -0300

    cuadrado en app.s se mueve y prende y apaga, prende y apaga y se mueve

--- proyecto2/app.s
+++ proyecto2/app.s
@@ -3,6 +3,7 @@ app:
 	// X0 contiene la direccion base del framebuffer
 	mov x3,x0 // copia del valor original del fb
 	mov x9,x0
+	mov x7,x0
 	//---------------- CODE HERE ------------------------------------
 
 	mov w10, 0xffff    // 0xffff = WHITE
@@ -19,7 +20,7 @@ loop0:
 
 	//---------------------------------------------------------------
 
-	mov x16,65536
+	mov x16,16384
 delay:
 	sub x16,x16,1
 	cbnz x16, delay 
@@ -32,34 +33,35 @@ delay:
 	add x0,x0,x4
 	add x0,x0,552
 	
-	mov w10, 0x1f
+	mov w13, 0x1f
 	mov x11,40
 	mov x12,40
 	// x10 va a ser el punto de inicio
 	// x11 va a ser el ancho --- eje X
 	// x12 va a ser el alto  --- eje Y
 
-cuadrado:
+// cuadrado:
 	// mov x3, x0     // inicio del cuadrado
-	mov x1,x11      // inicio de X
-	mov x2,x12      // inicio de Y
+	// mov x1,x11      // inicio de X
+	// mov x2,x12      // inicio de Y
 
 loop_ini:			// ciclo inicia X
 	add x0 , x0, 1024
 	sub x0, x0, 80
-    	mov x1 , x11
+    	mov x11 , 40
 
 loop_pinta:
-    	sturh w10,[x0]     // Pinta el pixel
+    	sturh w13,[x0]     // Pinta el pixel
     	add x0,x0,2	   // Next pixel
-	sub x1,x1,1	   // decrement X counter
-	cbnz x1,loop_pinta	   // If not end row jump
-	sub x2,x2,1	   // Decrement Y counter
-	cbnz x2,loop_ini	   // if not last row, jump
-	add x9, x9, 2
+	sub x11,x11,1	   // decrement X counter
+	cbnz x11,loop_pinta	   // If not end row jump
+	sub x12,x12,1	   // Decrement Y counter
+	cbnz x12,loop_ini	   // if not last row, jump
+	add x9, x9, 32
 	mov x3, x9
-	mov x16,65536
-	cbz x2, delay
+	mov x2,512
+	mov x0,x7
+	cbz x12, loop1
         // Infinite Loop
 InfLoop:
 	b InfLoop
